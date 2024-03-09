4a4333ba71927478848a900b94300c51c58a82d7
LordSilverThief1 <78735469+LordSilverThief1@users.noreply.github.com>
2024-03-09 15:34:39 -0500

    Transforming to php
    updating pages to be into php files, allowing the login, register page, reset password to function, along with tweaks to the database

--- Senior Project/Old Stuff/config.php
+++ Senior Project/Old Stuff/config.php
@@ -1,19 +1,19 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-const DB_SERVER = 'localhost';
-const DB_USERNAME = 'bonfire_user';
-const DB_PASSWORD = 'Password123';
-const DB_NAME = 'bonfire';
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'senior_project');
 
 /* Attempt to connect to MySQL database */
-function getDBConnection(){
+function getDB(){
     try{
         $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
         // Set the PDO error mode to exception
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
-    } catch(PDOException $e){
+    } catch(PDOException $e) {
         die("ERROR: Could not connect. " . $e->getMessage());
     }
 }
