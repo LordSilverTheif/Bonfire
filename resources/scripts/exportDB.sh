fb88ea28821f280083eeeb455ecb0f53fc7ac834
georgesalayka <george.salayka@gmail.com>
2024-04-10 10:27:40 -0400

    Added newest Database

--- resources/scripts/exportDB.sh
+++ resources/scripts/exportDB.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DATE=$(date +%d-%b-%y-%H_%M)
-mysqldump -ubonfire_user -pPassword123 -d -B --add-drop-database bonfire > ../database/dump_${DATE}.sql
+mysqldump -ubonfire_user -pPassword123 --opt bonfire > ../database/dump_${DATE}.sql
