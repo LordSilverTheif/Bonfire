a0b6d06f4466d6d6a818ce1f84880015416cc9d6
Ken Aebly <aeblyk@gmail.com>
2024-05-11 10:40:16 -0400

    Final pre merge change for assignments

--- classTopMenu.php
+++ classTopMenu.php
@@ -1,3 +1,16 @@
+<?php
+// Initialize the session
+session_start();
+require_once "config.php";
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+$role = $_SESSION["role"];
+
+?>
+
 <nav class="navbar bg-dark  bg-gradient">
 <div class="container">
     <header class="d-flex justify-content-center py-3">
@@ -5,11 +18,21 @@
             <li class="nav-item btn btn-outline-secondary"><a href="#" class="nav-link" aria-current="page">Class Home</a></li>
             <li class="nav-item btn btn-outline-secondary""><a href="#" class="nav-link">Announcements</a></li>
             <li class="nav-item btn btn-outline-secondary""><a href="assignments.php?classid=<?= $_SESSION["currentclass"] ?>" class="nav-link">Assignments</a></li>
+
+            <li class="nav-item btn btn-outline-secondary""><a href="#" class="nav-link">People</a></li>
             <?php if($role == "teacher" || $role == "admin"){ ?> <li  class="nav-item btn btn-outline-secondary""><a href="viewStudentGrades.php?classid=<?= $_SESSION["currentclass"] ?>" class="nav-link">Grades </li></a></li> <?php } ?>
             <?php if($role == "student"){ ?> <li  class="nav-item btn btn-outline-secondary""><a href="viewGrades.php?classid=<?= $_SESSION["currentclass"] ?>" class="nav-link">Grades </li></a></li> <?php } ?>
             <li class="nav-item btn btn-outline-secondary""><a href="classRoster.php?classid=<?= $_SESSION["currentclass"] ?>" class="nav-link">People</a></li>
             <li class="nav-item btn btn-outline-secondary""><a href="syllabus.php?classid=<?= $_SESSION["currentclass"] ?> "class="nav-link">Syllabus</a></li>
             <li class="nav-item btn btn-outline-secondary""><a href="#" class="nav-link">Files</a></li>
+            <?php
+            if($role == "teacher")
+            {
+                ?>
+                <li class="nav-item btn btn-outline-secondary""><a href="classSettings.php?classid=<?= $_SESSION["currentclass"] ?>" class="nav-link">settings</a></li>
+                <?php
+            }
+            ?>
         </ul>
     </header>
 </div>
